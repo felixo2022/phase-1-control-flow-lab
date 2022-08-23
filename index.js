3b6713134b7e9fe23cbc29a43c61099cb82dbffe
felixo2022 <walterbarax@gmail.com>
2022-08-24 00:57:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  // Writ
+  if( ride <= 400){
+    return 'This one is on me!'
+  }
+  if(ride >= 2000 && ride <= 2500){
+    return `I will gladly take your thirty bucks.`
+  }if ( ride >= 2500){
+    return `No can do.`
+}
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+  // Write your code here!\
+  
+    
+  }
+
+    
+  
+    
+  
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
